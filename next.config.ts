1555bf6ff6c704e19c66a19dd5ef6d638b628012
MM Digital <info@mm-digi.co.uk>
2026-09-21 15:45:23 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "mm-digi.co.uk" },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/email-signature/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
